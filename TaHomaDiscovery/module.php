660c9ad247a7f16272560d42edf91a133d3ff494
Michael Maroszek <paresy@gmail.com>
2022-07-13 20:40:14 +0200

    fix discovery

--- TaHomaDiscovery/module.php
+++ TaHomaDiscovery/module.php
@@ -9,13 +9,13 @@ class TaHomaDiscovery extends IPSModule
 
         foreach ($this->discoverDevices() as $device) {
             $data->actions[0]->values[] = [
-                'address'    => $device['Host'],
+                'address'    => $device['IPv4'],
                 'name'       => $device['Host'],
                 'pin'        => $device['PIN'],
-                'instanceID' => 0,
+                'instanceID' => $this->searchDevice($device['PIN']),
                 'create'     => [
                     [
-                        'moduleID'      => '{0AEE6E12-86BF-D8A1-90EA-66C9D45D052E}',
+                        'moduleID'      => '{AFAC39AA-770E-2AD7-0C24-D813F5FDC0FB}',
                     ],
                     [
                         'moduleID'      => '{161B0F84-1B8B-2EF0-1C8F-2EFFAC39006E}',
@@ -36,6 +36,18 @@ class TaHomaDiscovery extends IPSModule
         $devices = ZC_QueryServiceType($ids[0], '_kizboxdev._tcp', '');
         $this->SendDebug('QueryServiceType', print_r($devices, true), 0);
         $result = [];
+
+        $getValue = function ($txtRecords, $name)
+        {
+            foreach ($txtRecords as $txtRecord) {
+                $parts = explode('=', $txtRecord);
+                if ($parts[0] == $name) {
+                    return $parts[1];
+                }
+            }
+            return '';
+        };
+
         foreach ($devices as $device) {
             $deviceInfo = ZC_QueryService($ids[0], $device['Name'], '_kizboxdev._tcp', 'local.');
             if ($deviceInfo) {
@@ -43,10 +55,21 @@ class TaHomaDiscovery extends IPSModule
                 $result[] = [
                     'Host' => $deviceInfo[0]['Host'],
                     'IPv4' => $deviceInfo[0]['IPv4'][0],
-                    'PIN'  => 'ToDo',
+                    'PIN'  => $getValue($deviceInfo[0]['TXTRecords'], 'gateway_pin'),
                 ];
             }
         }
         return $result;
+    }
+
+    private function searchDevice($pin)
+    {
+        $ids = IPS_GetInstanceListByModuleID('{161B0F84-1B8B-2EF0-1C8F-2EFFAC39006E}');
+        foreach ($ids as $id) {
+            if (IPS_GetProperty($id, 'GatewayPIN') == $pin) {
+                return $id;
+            }
+        }
+        return 0;
     }
 }
