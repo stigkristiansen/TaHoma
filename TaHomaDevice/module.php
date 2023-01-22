c7daea2ec2c1d3d972a4244b1f24b8c73980dc00
stigkristiansen <stigkristiansen@outlook.com>
2023-01-22 16:54:12 +0100

    Update module.php

--- TaHomaDevice/module.php
+++ TaHomaDevice/module.php
@@ -19,6 +19,7 @@ class TaHomaDevice extends IPSModule
             IPS_SetVariableProfileAssociation('TAHOMA.OpenClosedState', 'open', $this->Translate('Offen'), 'Window-0', -1);
             IPS_SetVariableProfileAssociation('TAHOMA.OpenClosedState', 'stop', $this->Translate('Stop'), '', -1);
             IPS_SetVariableProfileAssociation('TAHOMA.OpenClosedState', 'closed', $this->Translate('Geschlossen'), 'Window-100', -1);
+            
             IPS_SetVariableProfileAssociation('TAHOMA.OpenClosedState', 'my', $this->Translate('My'), 'Window-0', -1);
             
         }
